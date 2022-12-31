f185245e43ddde506190b9b44e3b46f0a436044a
yogi <moh.yogi10065@gmail.com>
2022-12-31 12:37:04 +0700

    update configuration

--- config/config.js
+++ config/config.js
@@ -11,24 +11,24 @@ const{
 
 module.exports = {
   "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
+    "username": 'xxxx'|| DB_USERNAME,
+    "password": 'xxxxx' || DB_PASSWORD,
+    "database": 'todo4' || DB_NAME,
+    "host": '172.17.0.1' || DB_HOST,
     "dialect": "mysql"
   },
   "test": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
+    "username": 'xxxx'|| DB_USERNAME,
+    "password": 'xxxxx' || DB_PASSWORD,
+    "database": 'todo4' || DB_NAME,
+    "host": 'todo4' || DB_NAME,
     "dialect": "mysql"
   },
   "production": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_NAME,
-    "host": DB_HOST,
+    "username": 'xxxx'|| DB_USERNAME,
+    "password": 'xxxxx' || DB_PASSWORD,
+    "database": 'todo4' || DB_NAME,
+    "host": 'todo4' || DB_NAME,
     "dialect": "mysql"
   }
 }
